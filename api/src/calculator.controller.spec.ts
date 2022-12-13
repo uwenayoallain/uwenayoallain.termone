039d40663b684684dc9ebb93dffbc24ba7732b6f
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-12-13 09:17:19 +0000

    testing backend

--- api/src/calculator.controller.spec.ts
+++ api/src/calculator.controller.spec.ts
@@ -16,42 +16,42 @@ describe('CalculatorController', () => {
 
   describe('addition', () => {
     it('should return the sum of two numbers', () => {
-      expect(calculatorController.operate({ operand1: 2, operand2: 2, operator: "+" })).toBe({ "calcResponse": 4 });
+      expect(calculatorController.operate({ operand1: 2, operand2: 2, operator: "+" })).toStrictEqual({ calcResponse: 4 });
     });
   });
 
   describe('subtraction', () => {
     it('should return the difference of two numbers', () => {
-      expect(calculatorController.operate({ operand1: 4, operand2: 2, operator: "-" })).toBe({ "calcResponse": 2 });
+      expect(calculatorController.operate({ operand1: 4, operand2: 2, operator: "-" })).toStrictEqual({ calcResponse: 2 });
     });
   });
 
   describe('multiplication', () => {
     it('should return the product of two numbers', () => {
-      expect(calculatorController.operate({ operand1: 4, operand2: 2, operator: "*" })).toBe({ "calcResponse": 8 });
+      expect(calculatorController.operate({ operand1: 4, operand2: 2, operator: "*" })).toStrictEqual({ calcResponse: 8 });
     });
   });
 
   describe('division', () => {
     it('should return the quotient of two numbers', () => {
-      expect(calculatorController.operate({ operand1: 4, operand2: 2, operator: "/" })).toBe({ "calcResponse": 2 });
+      expect(calculatorController.operate({ operand1: 4, operand2: 2, operator: "/" })).toStrictEqual({ calcResponse: 2 });
     });
   });
 
   describe("power", () => {
     it('should return the power multiplication of two numbers', () => {
-      expect(calculatorController.operate({ operand1: 2, operand2: 3, operator: "**" })).toBe({ "calcResponse": 8 });
+      expect(calculatorController.operate({ operand1: 2, operand2: 3, operator: "**" })).toStrictEqual({ calcResponse: 8 });
     });
   })
 
   describe("log 10", () => {
     it('should return the logarithmn of two numbers', () => {
-      expect(calculatorController.operate({ operand1: 2, operand2: 1, operator: "log" })).toBe({ "calcResponse": 0 });
+      expect(calculatorController.operate({ operand1: 2, operand2: 1, operator: "log" })).toStrictEqual({ calcResponse: 0 });
     });
   })
   describe("ln", () => {
     it('should return the natural log(log(e)) of two numbers', () => {
-      expect(calculatorController.operate({ operand1: 2, operand2: 1, operator: "ln" })).toBe({ "calcResponse": 0 });
+      expect(calculatorController.operate({ operand1: 2, operand2: 1, operator: "ln" })).toStrictEqual({ calcResponse: 0 });
     });
   })
 });
